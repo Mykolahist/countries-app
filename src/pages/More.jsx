7cc565a0905c30b61a0a07727ddae1db0df67362
Mykolahist <frichmykola@gmail.com>
2022-06-12 00:50:52 +0300

    works with router dom 6

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -1,5 +1,28 @@
-export const More = ({match}) => {
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { searchByCountry } from "../configs";
+
+export const More = ({ match }) => {
+  const { name } = useParams();
+  const navigate = useNavigate();
+  navigate(-1);
+  const [country, setCountry] = useState(null);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)
+      .then(({data}) => setCountry(data[0])));
+  }, [name]);
+  
   return (
-    <div>More</div>
+    <div>
+      <button onClick={navigate}>
+        <IoArrowBack /> Back
+      </button>
+      {name}
+    </div>
   );
 };
